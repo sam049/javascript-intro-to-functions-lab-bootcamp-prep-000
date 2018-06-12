a498eee68909d5ff2e118ee5118cc4e55ab76cfe
Sam Miller <sammillerks@gmail.com>
2018-06-12 13:48:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,4 +19,6 @@ var uppercase = "Hello!"
 var lowercase = "hello"
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase === string)
+  if (string.toLowerCase === string) {
+    return "I can't hear you!"
+  }
